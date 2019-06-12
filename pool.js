d5260b71f18ae15966b829b57593c9b95022cf08
qiangzi <1015864208@qq.com>
2019-06-12 14:45:42 +0800

    修改连接池

--- pool.js
+++ pool.js
@@ -1,11 +1,11 @@
 //创建mysql连接池
 const mysql = require('mysql');
 var pool = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'speed',
-  connectionLimit: 10
+    host: 'w.rdc.sae.sina.com.cn',
+    user: '5n0l1o421y',
+    password: '4hy404lzl45zj4ykw0kh0zw5y451ih555jlm5xxh',
+    database: 'app_syspeed',
+    connectionLimit: 10
 });
 //把创建好的连接池导出
 module.exports = pool;
